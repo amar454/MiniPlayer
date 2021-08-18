0eec36c3209f937bf2ad1ca8a30665908d61fbfd
amar545 <aaamargml@gmail.com>
2021-08-17 23:28:59 -0700

    change songs setup start

---
+++ main.py
@@ -0,0 +1,9 @@
+# imports
+import os
+
+class Setup:
+    def __init__(self):
+        if os.path.isdir(os.path.abspath(os.path.dirname(__file__))):
+            self.SongsExist = False
+        i
+        
